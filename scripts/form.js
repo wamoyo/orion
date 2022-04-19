6b1a8a2d6515842a5e613bdb8c3350159625db84
Costa Michailidis <costa@innovationbound.com>
2022-04-19 10:58:59 -0400

    how about that?

--- scripts/form.js
+++ scripts/form.js
@@ -4,9 +4,14 @@
 
     var form = document.getElementById('contact-form')
     var fieldset = form.querySelector('fieldset')
+    var se = document.getElementById('bg')
 
     // Enabled form
     fieldset.removeAttribute('disabled')
+
+    // Show elevator
+    bg.classList.remove('hidden')
+
 
   })
 }());
