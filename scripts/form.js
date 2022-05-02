892537efaddebbcf71c2d6203dd4184a02437cbb
Costa Michailidis <costa@innovationbound.com>
2022-05-02 14:19:19 -0400

    animated banner entry

--- scripts/form.js
+++ scripts/form.js
@@ -4,13 +4,13 @@
 
     // var form = document.getElementById('contact-form')
     // var fieldset = form.querySelector('fieldset')
-    var rise = document.getElementById('rise')
+    var rise = document.getElementById('banner')
 
     // Enabled form
     // fieldset.removeAttribute('disabled')
 
     // Show elevator
-    rise.classList.remove('hidden')
+    rise.classList.remove('bghidden')
 
 
   })
